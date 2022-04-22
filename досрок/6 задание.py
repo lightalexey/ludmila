fbd7b4b938770db7e019e0ba92293d78a2bb82a3
Людмила Митилева <96296546+Ludok1610@users.noreply.github.com>
2022-04-22 15:58:24 +0300

    /

--- досрок/6 задание.py
+++ досрок/6 задание.py
@@ -1,7 +1,9 @@
-s=int(input())
-s=(s-10)//7
-n=1
-while s>0:
-    n=n*2
-    s=s-n
-print(n)
+for s in range(1,100):
+    k=s
+    s=(s-10)//7
+    n=1
+    while s>0:
+        n=n*2
+        s=s-n
+    if n==8:
+        print(k)
